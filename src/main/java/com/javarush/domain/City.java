0b647dcfa29b0e211d7e1a791c6e8ab25f3fbcae
lexink <lexink@mail.ru>
2023-02-08 03:46:38 +0300

    Complete final project

---
+++ src/main/java/com/javarush/domain/City.java
@@ -0,0 +1,27 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(schema = "world", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "district")
+    private String district;
+
+    @Column(name = "population")
+    private Integer population;
+}
